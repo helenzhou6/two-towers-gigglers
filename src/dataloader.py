9fcc73fc0fe97b6ce8c9fbba2c4d63bbc4527dec
root <root@Iohn2.>
2025-06-16 16:59:15 +0100

    Dataloader function

--- src/dataloader.py
+++ src/dataloader.py
@@ -5,12 +5,12 @@ from fasttext.FastText import tokenize
 
 class KeyQueryDataset(IterableDataset):
 
-    def __init__(self,   word2idx =None, num_negative_samples = 1, query_data_file= 'data/query.parquet', 
-                 docs_data_file = 'data/docs.parquet', ):
+    def __init__(self,   word2idx =None,  query_data_file= 'data/query.parquet', 
+                 docs_data_file = 'data/docs.parquet', ): #num_negative_samples = 1,
         super().__init__()
-    
+        #TODO: Allow num_negative_samples to be set bigger than one
         self.word2idx = word2idx
-        self.num_neg_samples = num_negative_samples
+        #self.num_neg_samples = num_negative_samples
         self.query_data = pd.load_parquet(query_data_file)
         self.doc_data = pd.load_parquet(docs_data_file)
     
@@ -19,10 +19,30 @@ class KeyQueryDataset(IterableDataset):
         #get positive sample
         query, pos_sample = tuple(self.query_data.sample(1).loc[:,["query", "doc"]])
         #get negative samples
-        neg_samples = list(self.doc_data.sample(self.num_neg_samples)["doc"])
+        neg_sample = self.doc_data.sample(1)["doc"][0]
         query_indices = torch.tensor([self.word2idx.get(token, '<UNK>') for token in tokenize(query)])
         pos_sample_indices = torch.tensor([self.word2idx.get(token, '<UNK>') for token in tokenize(pos_sample)])
-        neg_samples_indices = [torch.tensor([self.word2idx.get(token, '<UNK>') for token in tokenize(neg_sample)]
-                       for neg_sample in neg_samples)]
-        #TODO: Transform to index before returning
-        yield (query_indices, pos_sample_indices, neg_samples_indices)
+        neg_sample_indices = torch.tensor([self.word2idx.get(token, '<UNK>') for token in tokenize(neg_sample)])
+
+        yield (query_indices, pos_sample_indices, neg_sample_indices)
+
+def collate_fn_emb_bag(data_items):
+    queries, pos_samples, neg_samples = zip(data_items)
+    cat_queries = torch.cat(queries)
+    cat_pos_samples = torch.cat(pos_samples)
+    cat_neg_samples = torch.cat(neg_samples)
+
+    query_lengths = torch.tensor([t.numel() for t in queries], dtype=torch.long)
+    # The offsets are the cumulative sum of the lengths, starting with 0
+    query_offsets = torch.cat([torch.tensor([0]), query_lengths.cumsum(dim=0)[:-1]])
+
+    pos_samples_lengths = torch.tensor([t.numel() for t in pos_samples], dtype=torch.long)
+    pos_samples_offsets = torch.cat([torch.tensor([0]), pos_samples_lengths.cumsum(dim=0)[:-1]])
+    
+    neg_samples_lengths = torch.tensor([t.numel() for t in neg_samples], dtype=torch.long)
+    neg_samples_offsets = torch.cat([torch.tensor([0]), neg_samples_lengths.cumsum(dim=0)[:-1]])
+
+    return ((cat_queries, query_offsets), (cat_pos_samples, pos_samples_offsets), (cat_neg_samples, neg_samples_offsets))
+
+def getKQDataLoader(dataset, batch_size):
+    return DataLoader(dataset=dataset, batch_size=batch_size, collate_fn=collate_fn_emb_bag)
