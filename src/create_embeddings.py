37edef48d365d33405c1fa629db6f6075934a3cc
Andrew Phillipo <andrewphillipo@gmail.com>
2025-06-17 11:13:33 +0100

    fasttext embeddings

--- src/create_embeddings.py
+++ src/create_embeddings.py
@@ -5,11 +5,14 @@ import pandas as pd
 from fasttext.FastText import tokenize
 from fasttext import util
 import json
+from tqdm import tqdm
 
 from wandb_utils import init_wandb, save_model
 
+tqdm.pandas()
+
 # === 0. Params ===
-MAX_VOCAB_SIZE = 20000
+MAX_VOCAB_SIZE = 100000
 EMBEDDING_DIM =  300
 QUERY_DATA_FILE_PATH = 'data/query.parquet'
 DOCS_DATA_FILE_PATH = 'data/docs.parquet'
@@ -17,39 +20,50 @@ DOCS_DATA_FILE_PATH = 'data/docs.parquet'
 init_wandb(None, None)
 
 util.download_model('en', if_exists='ignore')  # English
-ft = fasttext.load_model('cc.en.300.bin')
+fasttext_model = fasttext.load_model('cc.en.300.bin')
 
 # === 1. Sample corpus ===
 # TODO: SWAP OUT THE BELOW!
+print('START: Concat Corpuses')
 corpus = pd.concat([pd.read_parquet(QUERY_DATA_FILE_PATH)["query"].drop_duplicates(), pd.read_parquet(DOCS_DATA_FILE_PATH)["doc"]])
+print('DONE: Concat Corpuses')
 
 # # === 2. Tokenize ===
-corpus_tokenized = corpus.apply(lambda row: tokenize(row)).sum()
+
+print('START: Tokenize Corpuses')
+corpus_tokenized = corpus.progress_apply(lambda row: tokenize(row)).explode().value_counts()
+print('DONE: Tokenize Corpuses')
 
 # # === 3. Count words and build vocabulary ===
-counter = Counter(corpus_tokenized)
-most_common = counter.most_common(MAX_VOCAB_SIZE)
-word2idx = {word: idx for idx, (word, _) in enumerate(most_common)}
+print('START: Count words and build most common vocab')
+most_common = corpus_tokenized.head(MAX_VOCAB_SIZE)
 
+word2idx = {}
+ind = 0
+for word in most_common.index.tolist():
+    word2idx[word] = ind
+    ind += 1
 word2idx['<UNK>'] = len(word2idx)  # Add unknown token
+print('END: Count words and build most common vocab')
 
-print(f"writing vocab.json for {MAX_VOCAB_SIZE}")
+print(f"START: writing vocab.json for {MAX_VOCAB_SIZE} words")
 with open("data/vocab.json", "w") as file:
     json.dump(word2idx, file)
 
 vocab_size = len(word2idx)
 print("Vocab size:", vocab_size)
+print(f"END: writing vocab.json for {MAX_VOCAB_SIZE} words")
 
-# # === 4. Load FastText model ===
-model = fasttext.load_model('cc.en.300.bin')  # Assumes you downloaded this
 
 # # === 5. Create fasttext_tensor ===
+print(f"START: create vectors for tokenized words")
 vectors = []
-for word in word2idx:
-    vec = model.get_word_vector(word)
+for word in tqdm(word2idx):
+    vec = fasttext_model.get_word_vector(word)
     vectors.append(torch.tensor(vec))
 fasttext_tensor = torch.stack(vectors)  # shape: [vocab_size, EMBEDDING_DIM]
 torch.save(fasttext_tensor, "data/fasttext_tensor.pt")
+print(f"DONE: create vectors for tokenized words")
 
 save_model('fasttext_tensor', 'FastText embedding from data set')
 
