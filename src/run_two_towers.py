a22d9cc4f8cd3a0d50577450f59248afa227581a
Andrew Phillipo <andrewphillipo@gmail.com>
2025-06-17 15:43:38 +0100

    added training params

--- src/run_two_towers.py
+++ src/run_two_towers.py
@@ -1,16 +1,16 @@
-import torch.nn.functional as F
 import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 import json
-from utils import load_model_path, init_wandb, get_device
+from utils import load_model_path, init_wandb, get_device, save_model
 from two_towers import QryTower, DocTower
-from dataloader import KeyQueryDataset, getKQDataLoader
+from dataloader import KeyQueryDataset, collate_fn_emb_bag, getKQDataLoader
 
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 EPOCHS = 5
-BATCH_SIZE = 3
-QUERY_END = 100_000
-MARGIN = 0.2
+BATCH_SIZE = 32
+QUERY_END = 10000
+MARGIN = torch.tensor(0.2)
 device = get_device()
 
 # Loads embeddings and test out works
@@ -22,53 +22,62 @@ embedding_bag = torch.nn.EmbeddingBag(num_embeddings, embedding_dim, mode="mean"
 embedding_bag.load_state_dict(ft_state_dict) # Returns Embedding(100001, 300)
 
 # Init Two Towers
-qryTower = QryTower(embedding_bag).to(device)
-docTower = DocTower(embedding_bag).to(device)
+query_model = QryTower(embedding_bag).to(device)
+doc_model = DocTower(embedding_bag).to(device)
 
-qry_optimizer = torch.optim.Adam(qryTower.parameters(), lr=LEARNING_RATE)
-doc_optimizer = torch.optim.Adam(docTower.parameters(), lr=LEARNING_RATE)
+qry_optimizer = torch.optim.Adam(query_model.parameters(), lr=LEARNING_RATE)
+doc_optimizer = torch.optim.Adam(doc_model.parameters(), lr=LEARNING_RATE)
 
 # May need to change the file path
 vocab_path = load_model_path('vocab:latest')
 with open(vocab_path) as file:
     w2ix = json.load(file)
 dataset = KeyQueryDataset(start=0, end=QUERY_END, word2idx=w2ix)
-train_dataloader = getKQDataLoader(dataset, batch_size=BATCH_SIZE)
+train_dataloader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, collate_fn=collate_fn_emb_bag)
 
-for epoch in range(1, EPOCHS+1):
+for epoch in range(0, EPOCHS):
     train_loss = 0.0
-    qryTower.train()
-    docTower.train()
+    query_model.train()
+    doc_model.train()
     for batch in tqdm(train_dataloader):
         query, pos_doc, neg_doc = batch
         query = (query[0].to(device), query[1].to(device))   # inputs, offsets
         pos_doc = (pos_doc[0].to(device), pos_doc[1].to(device))
         neg_doc = (neg_doc[0].to(device), neg_doc[1].to(device))
 
-        qry_vec = qryTower(query)
-        pos_vec = docTower(pos_doc)
-        neg_vec = docTower(neg_doc)
+        qry_vec = query_model(query)
+        pos_vec = doc_model(pos_doc)
+        neg_vec = doc_model(neg_doc)
 
         # Compute similarity scores
         pos_score = torch.nn.functional.cosine_similarity(qry_vec, pos_vec)
         neg_score = torch.nn.functional.cosine_similarity(qry_vec, neg_vec)
 
         # Difference between the two scores
-        dst_dif = pos_score - neg_score
-        # Margin
-        dst_mrg = torch.tensor(0.2)
-
-        # Loss function
-        # Makes the negative doc within the margin
-        loss = torch.max(torch.tensor(0.0), dst_mrg - dst_dif)
-        # To create the gradients for training
+        distance_scores = pos_score - neg_score
+        loss = torch.max(torch.tensor(0.0), MARGIN - distance_scores)
 
         # Backward
         qry_optimizer.zero_grad()
         doc_optimizer.zero_grad()
+
+        # loss from whole batch average
         loss = loss.mean()
         loss.backward()
+
+        for name, param in query_model.named_parameters():
+          if param.grad is None:
+              print(f"No grad for {name}")
+          else:
+              print(f"Grad for {name}: {param.grad.norm()}")
+
         qry_optimizer.step()
         doc_optimizer.step()
-
+        
         print(f"Epoch {epoch+1} complete. Loss: {loss.item():.4f}")
+
+torch.save(query_model.state_dict(), 'data/query_model.pt')
+save_model('query_model', 'The trained model for our queries')
+
+torch.save(doc_model.state_dict(), 'data/doc_model.pt')
+save_model('doc_model', 'The trained model for our documents')
