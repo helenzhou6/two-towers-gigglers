13d1845f989e53b0c91e56da1126f67fe73d1fc5
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-20 14:38:06 +0100

    Remove files not being actively used

--- src/tests/test_inference.py
+++ src/tests/test_inference.py
@@ -1,5 +1,4 @@
 from inference import search_query
-from inference_manual import search_query as search_query_manual
 
 # TO TEST, run:
 # `export REDIS_HOST=localhost` & `export PYTHONPATH=./src`
@@ -8,10 +7,5 @@ NUM_DOCS = 3
 
 print("----- REDIS RESULTS ------")
 results = search_query(QUERY, NUM_DOCS)
-for result in results:
-    print(result)
-
-print("----- MANUAL (pre redis) RESULTS ------")
-results = search_query_manual(QUERY, NUM_DOCS)
 for result in results:
     print(result)
