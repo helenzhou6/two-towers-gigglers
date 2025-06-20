1cb9dd9a2c1a4bf1c31aabea41d5321fa1173e94
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-20 12:19:33 +0100

    Update test_inference to include pre redis and post redis

--- src/tests/test_inference.py
+++ src/tests/test_inference.py
@@ -1,7 +1,15 @@
 from inference import search_query
+from inference_manual import search_query as search_query_manual
 
 # TO TEST, run:
 QUERY = "home pickled eggs causing botulism at room temperature"
 results = search_query(QUERY)
+print("----- REDIS RESULTS ------")
+
+for result in results:
+    print(result)
+
+results = search_query_manual(QUERY)
+print("----- MANUAL (pre redis) RESULTS ------")
 for result in results:
     print(result)
